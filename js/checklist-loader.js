475300d33fdf2ccf3c3e334494285de860bcf389
MWUlsh <matthew.ulsh@fulbrightmail.org>
2025-07-24 22:39:10 +0800

    insert bug tracker js code
    looking for bugs w/ temp code

--- js/checklist-loader.js
+++ js/checklist-loader.js
@@ -1,4 +1,8 @@
 export async function loadChecklist() {
+  try {
+    const response = await fetch('data/checklist.csv');
+    if (!response.ok) throw new Error('Checklist CSV not found');
+    const text = await response.text();
   const response = await fetch('data/checklist.csv');
   const text = await response.text();
 
--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ import { loadSDOH } from './sdoh-loader.js';
 import { setupConsent } from './consent.js';
 import { setupLanguage } from './i18n.js';
 
+console.log('Main.js loaded');
+
 document.addEventListener('DOMContentLoaded', async () => {
+  console.log('DOM fully loaded');
   setupLanguage();
   setupConsent();
   await loadChecklist();
